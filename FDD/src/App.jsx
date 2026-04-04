3ba0740426834a30b22c5149b6d640cf276771ac
PIYUSH2800 <singhpiyush3379@gmail.com>
2026-04-04 09:43:01 +0530

    Undo Subscription feature

--- FDD/src/App.jsx
+++ FDD/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
-import { SubscriptionProvider } from './context/SubscriptionContext';
 import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
@@ -10,11 +9,6 @@ import DonorDashboard from './pages/DonorDashboard';
 import NGODashboard from './pages/NGODashboard';
 import AdminDashboard from './pages/AdminDashboard';
 import LiveFeed from './pages/LiveFeed';
-import SubscriptionPlans from './pages/SubscriptionPlans';
-import SubscriptionHistory from './pages/SubscriptionHistory';
-import AnimalShelterDashboard from './pages/AnimalShelterDashboard';
-import CompostUnitDashboard from './pages/CompostUnitDashboard';
-import Community from './pages/Community';
 
 function ProtectedRoute({ children, allowedRoles }) {
   const { user } = useAuth();
@@ -44,45 +38,30 @@ export default function App() {
   };
 
   return (
-    <SubscriptionProvider>
-      <BrowserRouter>
-        <Navbar theme={theme} onToggleTheme={toggleTheme} />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/feed" element={<LiveFeed />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/subscription-plans" element={<SubscriptionPlans />} />
-          <Route path="/subscription-history" element={<SubscriptionHistory />} />
-          <Route path="/donor" element={
-            <ProtectedRoute allowedRoles={['donor']}>
-              <DonorDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/ngo" element={
-            <ProtectedRoute allowedRoles={['ngo', 'animal_shelter', 'compost_unit']}>
-              <NGODashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/compost" element={
-            <ProtectedRoute allowedRoles={['compost_unit']}>
-              <CompostUnitDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/shelter" element={
-            <ProtectedRoute allowedRoles={['animal_shelter']}>
-              <AnimalShelterDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/admin" element={
-            <ProtectedRoute allowedRoles={['admin']}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </SubscriptionProvider>
+    <BrowserRouter>
+      <Navbar theme={theme} onToggleTheme={toggleTheme} />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/feed" element={<LiveFeed />} />
+        <Route path="/donor" element={
+          <ProtectedRoute allowedRoles={['donor']}>
+            <DonorDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/ngo" element={
+          <ProtectedRoute allowedRoles={['ngo', 'animal_shelter', 'compost_unit']}>
+            <NGODashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/admin" element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <AdminDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
